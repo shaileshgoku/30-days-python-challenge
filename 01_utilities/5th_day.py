f054e30f9b5f8489079c9ec12282a1a5c3b715ae
SHAILESH S <100753992+shaileshgoku@users.noreply.github.com>
2025-12-27 20:05:31 +0530

    Add files via upload

---
+++ 01_utilities/5th_day.py
@@ -0,0 +1,50 @@
+"""
+ Challenge: Emoji Enhancer for Messages
+
+Create a Python script that takes a message and adds emojis after specific keywords to make it more expressive.
+
+Your program should:
+1. Ask the user to input a message.
+2. Add emojis after certain keywords (like "happy", "love", "code", "tea", etc.).
+3. Print the updated message with emojis.
+
+Example:
+Input:
+  I love to code and drink tea when I'm happy.
+
+Output:
+  I love ❤️ to code 💻 and drink tea 🍵 when I'm happy 😊.
+
+Bonus:
+- Make it case-insensitive (match "Happy" or "happy")
+- Handle punctuation (like commas or periods right after keywords)
+
+"""
+# get a dictionary
+emoji_map_fun = {
+    "love": "❤",
+    "happy": "☺",
+    "code": "💻",
+    "tea": "☕",
+    "music": "🎵",
+    "food": "🥗"
+}
+
+
+#get user message
+message = input("Enter your message: ")
+
+updates_words = []
+# process each word
+for word in message.split():
+    cleaned = word.lower().strip(".,!?")
+    emoji = emoji_map_fun.get(cleaned, "")
+    if emoji:
+        updates_words.append(f"{word} {emoji}")
+    else:
+        updates_words.append(word)
+
+updated_message = " ".join(updates_words)
+
+print("\n Enhanced message:\n")
+print(updated_message)
